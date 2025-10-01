31c096fa65c88cdcc69128dcb1fec2601840f593
OlegLyan <oleg.lyan2801@gmail.com>
2025-10-01 15:12:23 +0300

    app

--- app.py
+++ app.py
@@ -5,23 +5,14 @@ import service
 def main():
     conn = db.connect()
     db.create_tables(conn)
-
-    # Добавляем книги и читателя
     repo.add_book(conn, "Война и мир", "Толстой", "Роман", 2)
     repo.add_book(conn, "Преступление и наказание", "Достоевский", "Роман", 1)
-
     pr = repo.add_reader(conn, "Иван Иванов", "89991112233", 25)
-
-    # Бронирование и выдача (дату передаём строкой)
     service.hold_book(conn, pr, "Война и мир", "Толстой", "01/10/25")
     service.loan_book(conn, pr, "Война и мир", "Толстой", "02/10/25")
     service.return_book(conn, pr, "Война и мир", "Толстой")
-
-    # Списки
     print("Книги на руках:", service.get_loans_by_reader(conn, pr))
     print("Забронированные:", service.get_holds_by_reader(conn, pr))
-
-    # Поиск
     print("Поиск по жанру 'Роман':", service.search(conn, genre="Роман"))
 
 if __name__ == "__main__":
